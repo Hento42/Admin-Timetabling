c0d69ff3d16dbf38da87e6c24d437417923debcd
Hento42 <hgthomas07@outlook.com>
2024-09-16 14:13:28 +0100

    Inserted data into the tables

--- database.py
+++ database.py
@@ -2,19 +2,27 @@ import sqlite3
 con = sqlite3.connect("databases.db") # Connecting to the database
 
 editor = con.cursor()   # Linking to the database
+
 editor.execute("CREATE TABLE StaffDetails ("
                 "StaffCode INTEGER PRIMARY KEY,"
                 "FirstName TEXT,"
                 "Surname TEXT,"
                 "Level INTEGER," 
                 "Email TEXT,"
-                "MonStart date,"
-                "MonFinish date,"
-                "TueStart date,"
-                "TueFinish date," 
-                "WedStart date,"
-                "WedFinish date,"
-                "ThurStart date,"
-                "ThurFinish date,"
-                "FriStart date,"
-                "FriFinish date)")
+                "MonStart time,"
+                "MonFinish time,"
+                "TueStart time,"
+                "TueFinish time," 
+              "WedStart time,"
+                "WedFinish time,"
+                "ThurStart time,"
+                "ThurFinish time,"
+               "FriStart time,"
+                "FriFinish time)")
+
+editor.execute("""INSERT INTO StaffDetails VALUES
+               (1, 'Arthur', 'Dent', 5, 'email@totallyrealemail.com', '10:00:00', '18:00:00', '10:00:00', '18:00:00', '10:00:00', '18:00:00', '10:00:00', '18:00:00', '10:00:00', '18:00:00'),
+               (2, 'Bowerick', 'Wowbagger', 10, 'anotheremail@totallyrealemail.com', '08:00:00', '15:00:00', '08:00:00', '15:00:00', '08:00:00', '15:00:00', '08:00:00', '15:00:00', '08:00:00', '15:00:00'),
+               (3, 'Zaphod', 'Beeblebrox', 1, 'president@theuniverse.com', '11:30:00', '14:00:00', '09:30:00', '10:00:00', '12:00:00', '15:00:00', '08:00:00', '09:00:00', '15:00:00', '16:00:00')""")
+
+con.commit()
