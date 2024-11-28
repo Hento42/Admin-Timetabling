81dd961cc9def44bb153fadd23c4f20ea7f5e3a0
Hento42 <hgthomas07@outlook.com>
2024-11-28 11:34:39 +0000

    Moved the schedule sub into scheduling.py

--- classes.py
+++ classes.py
@@ -467,28 +467,6 @@ def linkJob():
 
     return jobs
 
-
-# Subprogram for the scheduling part of the program, so it can be imported into and called by main.py
-def schedule(pJobs, pStaff, pHours, day):
-    maxPriority = 0
-    priorities = [record[4] for record in pJobs]    # Gets a list of all the priorities
-    mergeSort(priorities)                           
-    maxPriority = priorities[-1]                    # Sorts the priorities and finds  the largest
-    jobStack = Stack([])
-
-    while maxPriority >= 0:
-        currentList = []
-        
-        for job in pJobs:
-            if job[4] == maxPriority:
-                currentList.append(job)     # Generates a 2D list for ech priority level
-                
-        jobStack.push(currentList)          # Pushes this list to the stack, then decreases the max priority
-        maxPriority -= 1
-    
-    # Placeholder for the scheduling algorithm        
-    def schedulingAlgorithm(pJobList, pStaffList, pHourList, pDay):
-        pass
     
     
 con.commit() # Commits all the changes from the program
